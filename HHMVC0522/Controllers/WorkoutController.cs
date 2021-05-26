21fcc667f203f36dee2cfd755909acd244ec5275
enqidong0207 <83167251+enqidong0207@users.noreply.github.com>
2021-05-27 00:17:09 +0800

    add jquery ui and timepicker addon

--- HHMVC0522/Controllers/WorkoutController.cs
+++ HHMVC0522/Controllers/WorkoutController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,37 @@ namespace UI.Controllers
 {
     public class WorkoutController : Controller
     {
-        // GET: Workout
+        HealthHelperEntities dbContext = new HealthHelperEntities();
+
         public ActionResult WorkoutLog()
         {
-            return View();
+            return View(dbContext.Workouts.ToList());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult GetWorkoutLog()
+        {
+            var q = dbContext.WorkoutLogs.Where(wl => wl.MemberID == 83)
+                .Select(wl => new
+                {
+                    wl.ID,
+                    wl.EditTime,
+                    wl.Workout.Name,
+                    wl.WorkoutTime,
+                    wl.WorkoutHours,
+                    Status = wl.Status.Name
+                });
+
+            return Json(q.ToList());
+        }
+
+        [HttpPost]
+        public JsonResult GetWorkoutAlWc(int wid)
+        {
+            var workout = dbContext.Workouts.SingleOrDefault(w => w.ID == wid);
+
+            return Json(new { Al = workout.ActivityLevel.Description, Wc = workout.WorkoutCategory.Name});
         }
     }
 }
