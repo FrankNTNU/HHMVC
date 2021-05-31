87383057ffd93e6fb8dfd38ff231de1ad9712ebd
enqidong0207 <83167251+enqidong0207@users.noreply.github.com>
2021-05-31 12:09:55 +0800

    WeightLog under construction

---
+++ HHMVC0522/Controllers/WeightController.cs
@@ -0,0 +1,44 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class WeightController : Controller
+    {
+        HealthHelperEntities db = new HealthHelperEntities();
+
+        // GET: Weight
+        public ActionResult WeightLog()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public JsonResult GetWeightLog()
+        {
+            var q = db.WeightLogs.Where(wgt => wgt.MemberID == 83).Select(wgt => new 
+            { 
+                wgt.ID,
+                wgt.UpdatedDate,
+                wgt.Weight
+            });
+
+            return Json(q.ToList());
+        }
+
+
+        public JsonResult GetWeightLogForChart()
+        {
+            var q = db.WeightLogs.Where(wgt => wgt.MemberID == 83).Select(wgt => wgt.Weight);
+
+            return Json(q.ToList());
+        }
+
+
+    }
+}
