7f1ba43289fb75b8f0d441d0284c048de15c79e6
enqidong0207 <83167251+enqidong0207@users.noreply.github.com>
2021-06-02 12:22:59 +0800

    for backup

--- HHMVC0522/Controllers/WeightController.cs
+++ HHMVC0522/Controllers/WeightController.cs
@@ -1,9 +1,11 @@
 ﻿using DAL;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 namespace UI.Controllers
 {
@@ -14,6 +16,17 @@ namespace UI.Controllers
         // GET: Weight
         public ActionResult WeightLog()
         {
+            DateTime startMonth = new DateTime(DateTime.Now.Year, 1, 1);
+            DateTime endMonth = DateTime.Now;
+
+            var q = from wgt in db.WeightLogs.Where(wgt => wgt.MemberID == 83).ToList()
+                    where startMonth <= wgt.UpdatedDate && wgt.UpdatedDate < endMonth
+                    group wgt by wgt.UpdatedDate.ToString("yyMM") into g
+                    orderby g.Key ascending
+                    select g.Average(wgt => wgt.Weight);
+
+            ViewBag.initialWeightLog = Newtonsoft.Json.JsonConvert.SerializeObject(q.ToList());
+
             return View();
         }
 
@@ -32,13 +45,86 @@ namespace UI.Controllers
         }
 
 
-        public JsonResult GetWeightLogForChart()
+        public JsonResult GetWeightLogForChart(DateTime startMonth, DateTime endMonth)
         {
-            var q = db.WeightLogs.Where(wgt => wgt.MemberID == 83).Select(wgt => wgt.Weight);
+            //var q = db.WeightLogs.Where(wgt => wgt.MemberID == 83).Select(wgt => wgt.Weight);
+
+            endMonth = endMonth.AddMonths(1);
+
+            var q = from wgt in db.WeightLogs.Where(wgt => wgt.MemberID == 83).ToList()
+                    where startMonth <= wgt.UpdatedDate && wgt.UpdatedDate < endMonth
+                    group wgt by wgt.UpdatedDate.ToString("yyMM") into g
+                    orderby g.Key ascending
+                    select g.Average(wgt => wgt.Weight);
 
             return Json(q.ToList());
+
+            //return Json(new { start = startMonth, end = endMonth });
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public JsonResult AddWeightLog(WeightLog wgtl)
+        {
+            DateTime today = DateTime.Now.Date;
 
+            if (db.WeightLogs
+                .Any(wgtl1 => DbFunctions.TruncateTime(wgtl1.UpdatedDate) == today))
+            {
+                return Json(new { Result = "failed", Error = "今天已新增紀錄過了" });
+            }
+
+            wgtl.MemberID = 83;
+            wgtl.UpdatedDate = DateTime.Now;
+
+            db.WeightLogs.Add(wgtl);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return Json(new { Result = "failed", Error = ex.Message });
+            }
+
+            return Json(new { Result = "success", Error = "none", ID = wgtl.ID});
+        }
+
+        public JsonResult EditWeightLog(WeightLog wgtlToEdit)
+        {
+            WeightLog wgtl = db.WeightLogs
+                .SingleOrDefault(wgtl1 => wgtl1.ID == wgtlToEdit.ID 
+                && wgtl1.MemberID == 83);
+
+            wgtl.Weight = wgtlToEdit.Weight;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return Json(new { Result = "failed", Error = ex.Message });
+            }
+
+            return Json(new { Result = "success", Error = "none"});
+        }
+
+        //======================================================
+        //Session Test
+
+        //public JsonResult SessionTest()
+        //{
+        //    int count = 0;
+        //    if (Session["count"] != null)
+        //    {
+        //        count = (int)Session["count"];
+        //    }
+        //    count++;
+        //    Session["count"] = count;
+
+        //    return Json(new { count = (int)Session["count"] });
+        //}
     }
 }
