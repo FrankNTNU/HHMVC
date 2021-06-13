f8aa7deba8832132dc788b595bf364fa32e35361
enqidong0207 <83167251+enqidong0207@users.noreply.github.com>
2021-06-13 16:24:43 +0800

    QnA Maker Service initially complete

--- HHMVC0522/Controllers/FrontContactController.cs
+++ HHMVC0522/Controllers/FrontContactController.cs
@@ -7,6 +7,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+//===============================
+//For QnA
+using Microsoft.Azure.CognitiveServices.Knowledge.QnAMaker;
+using Microsoft.Azure.CognitiveServices.Knowledge.QnAMaker.Models;
+using System.Threading.Tasks;
 
 namespace UI.Controllers
 {
@@ -27,9 +32,10 @@ namespace UI.Controllers
             return View();
         }
 
-
+        //=======================================================================
+        //恩旗
         [HttpPost]
-        public ActionResult Chat(string connId, string message)
+        public async Task<ActionResult> Chat(string connId, string message)
         {
             HealthHelperEntities dbContext = new HealthHelperEntities();
             Random rn = new Random();
@@ -40,15 +46,54 @@ namespace UI.Controllers
                 UserID = int.Parse(cu.UserID)
             }).Where(cu => dbContext.Members.SingleOrDefault(cu1 => cu1.ID == cu.UserID).IsAdmin)
                 .Select(cu => cu.ConnID).ToList();
-            
-            //todo AI認知服務
 
             var context = GlobalHost.ConnectionManager.GetHubContext<ChatHub>();
+
+            //todo AI認知服務
+            if (adminConnId.Count == 0)
+            {
+                string answer = await GetAnsFromKB(message);
+                context.Clients.Client(connId).ReceiveFromService(answer);
+                return View();
+            }
 
             context.Clients.Client(adminConnId[rn.Next(0, adminConnId.Count)])
                 .ReceiveFromCustomer(connId, Session["UserName"], Session["ImagePath"], message);
 
             return View();
         }
+
+        //==============================================================
+        //恩旗
+        //GetAnswers From Knowledge Base
+
+        private async Task<string> GetAnsFromKB(string message) {
+
+            string kbId = "acd4f593-dc0c-43ef-b658-77546c18ef5f";
+            var authoringKey = "b2ab1e830f1548c38bec03719a4bcabf";
+            var authoringURL = "https://msit130myqnamaker.cognitiveservices.azure.com/";
+            var queryingURL = "https://msit130myqnamaker.azurewebsites.net";
+
+            //===============================================================
+            //Get QueryKey
+            var client = new QnAMakerClient(new ApiKeyServiceClientCredentials(authoringKey))
+            { Endpoint = authoringURL };
+
+            var endpointKeysObject = await client.EndpointKeys.GetKeysAsync();
+            var primaryQueryEndpointKey = endpointKeysObject.PrimaryEndpointKey;
+
+            //================================================================
+            //New RuntimeClient
+            var runtimeClient = new QnAMakerRuntimeClient(new EndpointKeyServiceClientCredentials(primaryQueryEndpointKey))
+            { RuntimeEndpoint = queryingURL };
+
+            //================================================================
+            //Get Answer from KB
+            var response = await runtimeClient.Runtime
+                .GenerateAnswerAsync(kbId, new QueryDTO { Question = message });
+
+            return response.Answers[0].Answer;
+        }
+
     }
 }
